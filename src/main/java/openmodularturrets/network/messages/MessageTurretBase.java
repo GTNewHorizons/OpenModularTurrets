11bf27909f6779be42dd1aa0308c18bc5d5c6dbd
Algent <algent@gmail.com>
2026-09-04 21:45:52 +0200

    fix(network): encode turret names as UTF-8
    Formatted display names contain multibyte characters. Character-count prefixes desynchronized the packet and disconnected clients.
    
    Refs GTNewHorizons/GT-New-Horizons-Modpack#26087

--- src/main/java/openmodularturrets/network/messages/MessageTurretBase.java
+++ src/main/java/openmodularturrets/network/messages/MessageTurretBase.java
@@ -81,10 +81,8 @@ public class MessageTurretBase implements IMessage {
         this.x = buf.readInt();
         this.y = buf.readInt();
         this.z = buf.readInt();
-        int ownerLength = buf.readInt();
-        this.owner = new String(buf.readBytes(ownerLength).array());
-        int ownerNameLength = buf.readInt();
-        this.ownerName = new String(buf.readBytes(ownerNameLength).array());
+        this.owner = ByteBufUtils.readUTF8String(buf);
+        this.ownerName = ByteBufUtils.readUTF8String(buf);
         this.rfStorage = buf.readInt();
         this.yAxisDetect = buf.readInt();
         this.attacksMobs = buf.readBoolean();
@@ -97,11 +95,9 @@ public class MessageTurretBase implements IMessage {
         int lengthOfTPList = buf.readInt();
         if (lengthOfTPList > 0) {
             for (int i = 0; i < lengthOfTPList; i++) {
-                int length = buf.readInt();
-                String name = new String(buf.readBytes(length).array());
+                String name = ByteBufUtils.readUTF8String(buf);
                 TrustedPlayer trustedPlayer = new TrustedPlayer(name);
-                length = buf.readInt();
-                trustedPlayer.uuid = UUID.fromString(new String(buf.readBytes(length).array()));
+                trustedPlayer.uuid = UUID.fromString(ByteBufUtils.readUTF8String(buf));
                 trustedPlayer.canOpenGUI = buf.readBoolean();
                 trustedPlayer.canChangeTargeting = buf.readBoolean();
                 trustedPlayer.admin = buf.readBoolean();
@@ -115,10 +111,8 @@ public class MessageTurretBase implements IMessage {
         buf.writeInt(x);
         buf.writeInt(y);
         buf.writeInt(z);
-        buf.writeInt(owner.length());
-        buf.writeBytes(owner.getBytes());
-        buf.writeInt(ownerName.length());
-        buf.writeBytes(ownerName.getBytes());
+        ByteBufUtils.writeUTF8String(buf, owner);
+        ByteBufUtils.writeUTF8String(buf, ownerName);
         buf.writeInt(rfStorage);
         buf.writeInt(yAxisDetect);
         buf.writeBoolean(attacksMobs);
@@ -131,10 +125,8 @@ public class MessageTurretBase implements IMessage {
         buf.writeInt(trustedPlayers.size());
         if (trustedPlayers.size() > 0) {
             for (TrustedPlayer trustedPlayer : trustedPlayers) {
-                buf.writeInt(trustedPlayer.getName().length());
-                buf.writeBytes(trustedPlayer.getName().getBytes());
-                buf.writeInt(trustedPlayer.uuid.toString().length());
-                buf.writeBytes(trustedPlayer.uuid.toString().getBytes());
+                ByteBufUtils.writeUTF8String(buf, trustedPlayer.getName());
+                ByteBufUtils.writeUTF8String(buf, trustedPlayer.uuid.toString());
                 buf.writeBoolean(trustedPlayer.canOpenGUI);
                 buf.writeBoolean(trustedPlayer.canChangeTargeting);
                 buf.writeBoolean(trustedPlayer.admin);
